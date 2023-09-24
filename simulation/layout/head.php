bee56f57902b4818690ea3328ff26b1b58f3dcec
m.matsumoto <m.matsumoto@lifeperformance.jp>
2023-09-25 08:19:07 +0900

    header/footer本番と同じく変更し、jqueryをコメントアウト　確認中

---
+++ simulation/layout/head.php
@@ -0,0 +1,52 @@
+<head>
+<meta charset="utf-8">
+<meta charset="UTF-8">
+<title>AZ-ONE株式会社｜シュミレーション</title>
+<meta name="keywords" content="エージーワン,AZ-ONE,マンション投資,マンション・アパート販売,首都圏,資産運用,不動産投資,節税">
+<meta name="description" content="お問い合わせについて。AZ-ONE（エージーワン）は住む人の心と生活を豊かにする、資産価値の高いマンション・アパートの販売事業を行っています。資産運用・資産形成をサポート致します。">
+<meta property="og:title" content="AZ-ONE株式会社｜お問合せ">
+<meta property="og:type" content="article">
+<meta property="og:url" content="http://az-one.biz/faq/">
+<meta property="og:image" content="http://az-one.biz/img/common/og_image.png">
+<meta property="og:site_name" content="AZ-ONE（エージーワン）は住む人の心と生活を豊かにする、資産価値の高いマンション・アパートの販売事業を行っています。資産運用・資産形成をサポート致します。">
+<meta property="og:description" content="お問い合わせについて。住む人の心と生活を豊かにする、資産価値の高いマンション・アパートの販売事業を行っています。資産運用・資産形成をサポート致します。">
+<meta name="twitter:card" content="summarylargeimage">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<link rel="canonical" href="https://az-one.biz/faq/" />
+<link rel="shortcut icon" href="/img/common/favicon.ico">
+<link rel="apple-touch-icon-precomposed" href="/img/common/apple-touch-icon.png" />
+<link href="https://fonts.googleapis.com/earlyaccess/notosansjp.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet" />
+<link href="../../css/reset.css" rel="stylesheet" type="text/css">
+<link href="../../css/common.css" rel="stylesheet" type="text/css">
+<link href="../../css/swiper.css" rel="stylesheet" type="text/css">
+<link href="../../css/fancybox.css" rel="stylesheet" type="text/css">
+<script src="../../js/jquery-1.12.4.js" type="text/javascript"></script>
+<script src="../../js/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="../../js/tableHeadFixer.js" type="text/javascript"></script>
+<script src="../../js/swiper.min.js" type="text/javascript"></script>
+<script src="../../js/jquery.fancybox.js" type="text/javascript"></script>
+<script src="../../js/common.js" type="text/javascript"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125268458-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125268458-1');
+</script>
+<script async src="https://beacon.digima.com/v2/bootstrap/d0b-WENaR1ZFUFNXS3x2SnI0RktVQ2hlbkhZT1pXbHlHUA"></script><!--az-one.biz 用のトラッキングタグ_2020-03-24 -->
+
+  <!-- ここからシュミレーション -->
+  <?php
+    $page = basename($_SERVER['PHP_SELF']); // 現在のページのファイル名を取得
+    if ($page == "index.php" && strpos($_SERVER['PHP_SELF'], "/ap/") !== false) {
+        echo '<link rel="stylesheet" media="all" type="text/css" href="../css/simulation.css" />';
+    } elseif ($page == "index.php" && strpos($_SERVER['PHP_SELF'], "/nk/") !== false) {
+        echo '<link rel="stylesheet" media="all" type="text/css" href="../css/simulation_nk.css" />';
+    } elseif ($page == "index.php" && strpos($_SERVER['PHP_SELF'], "/ms/") !== false) {
+        echo '<link rel="stylesheet" media="all" type="text/css" href="../css/simulation_ms.css" />';
+    }
+  ?>
+</head>
--- simulation/nk/index.php
+++ simulation/nk/index.php
@@ -8,55 +8,14 @@ require_once "../PHPExcel/Classes/PHPExcel.php";
 <html>
 <body>
 
-<header>
-    <meta charset="utf-8">
-    <title>シミュレーション結果</title>
-    <meta name="viewport" content="width=device-width,user-scalable=yes,maximum-scale=1.6" />
-    <!-- ※デフォルトのスタイル（style.css） -->
-    <link rel="stylesheet" media="all" type="text/css" href="../css/style_pc.css" />
-    <!-- ※スマートフォン用のスタイル（smart.css） -->
-    <link rel="stylesheet" media="all" type="text/css" href="../css/style_sp.css" />
-    <!-- ※シュミレーション用のスタイル（smart.css） -->
-    <link rel="stylesheet" media="all" type="text/css" href="../css/simulation_nk.css" />
-    <!-- ※drawerのスタイル -->
-    <!-- ※drawerのスタイル -->
-    <link rel="stylesheet" media="all" type="text/css" href="../css/drawer.css">
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="../js/jquery.drawer.js"></script>
-    <script type="text/javascript" src="../js/1.10.1_jquery.min.js"></script>
-    <script type="text/javascript" src="../js/acor.js"></script>
-    <div class="inner">
-        <h1 class="pcView"><a href="/"><img src="../img/common/logo.svg" width="80%" height="" alt="AZEST-GROUP" /></a></h1>
-        <h1 class="spView"><a href="/"><img src="../img/common/logo.svg" width="318" height="60" alt="AZEST-GROUP" /></a></h1>
-        <nav class="pcView">
-            <ul>
-                <!-- <li class="message"><a href="/message/">代表ご挨拶</a></li> -->
-                <li class="identity"><a href="/identity/">企業理念</a></li>
-                <li class="overview"><a href="/overview/">会社概要</a></li>
-                <li class="asset"><a href="/asset/">資産運用</a></li>
-                <li class="reason"><a href="/reason/">なぜ不動産投資なのか</a></li>
-                <li class="information"><a href="/information/">イベント情報</a></li>
-                <li class="contact"><a href="/faq/">お問合せ</a></li>
-            </ul>
-        </nav>
-        <div class="drawer drawer-right spView">
-            <button  class="drawer-toggle drawer-hamburger"> <span class="sr-only">toggle navigation</span> <span class="drawer-hamburger-icon"></span> </button>
-            <div class="drawer-main drawer-default">
-                <nav class="drawer-nav" role="navigation">
-                    <ul class="nav drawer-nav-list">
-                        <li><a href="/">TOP</a></li>
-                        <li class="identity"><a href="/identity/">企業理念</a></li>
-                        <li class="overview"><a href="/overview/">会社概要</a></li>
-                        <li class="asset"><a href="/asset/">資産運用</a></li>
-                        <li class="reason"><a href="/reason/">なぜ不動産投資なのか</a></li>
-                        <li class="information"><a href="/information/">イベント情報</a></li>
-                        <li class="contact"><a href="/faq/">お問合せ</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
+<?php
+include('../layout/head.php');
+?>
+<body>
+
+<?php
+include('../layout/header.php');
+?>
 
 
 <div  id="content">
@@ -142,10 +101,10 @@ require_once "../PHPExcel/Classes/PHPExcel.php";
         </div>
     </section>
 
-    <script
+    <!-- <script
             src="https://code.jquery.com/jquery-3.4.1.js"
             integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous"></script> -->
     <script src="../js/jquery.validate.min.js" type="text/javascript"></script>
     <script>
 
